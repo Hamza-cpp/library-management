f706a2082165d74e2a12aa8ddc0b32f55c084dc0
Hamza-Cpp <hamza.okhadir2018@gmail.com>
2024-02-04 00:42:56 +0100

    some changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
-#include "./include/models/Book.h"
-#include "./include/models/User.h"
-#include "./include/controllers/BookController.h"
-#include "./include/controllers/UserController.h"
+#include "crow.h"
 
-int main() {
-    std::cout << "Welcome to the Library Management System!" << std::endl;
-    // TODO: Bootstrapping code goes here
-    return 0;
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    app.port(18080).run();
 }
